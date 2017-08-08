1cd54a38dd8263e0e257f0cc123cdde44a41a07f
Damien Marest <damien.marest@gmail.com>
2017-08-08 16:40:37 +0200

    feat(ControlScaleLineComponent): add inputs and change behaviour on scale line control component

--- src/components/controls/scaleline.component.ts
+++ src/components/controls/scaleline.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { control } from 'openlayers';
+import { Component, OnDestroy, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { control, MapEvent } from 'openlayers';
 import { MapComponent } from '../map.component';
 
 @Component({
   selector: 'aol-control-scaleline',
   template: `<ng-content></ng-content>`
 })
-export class ControlScaleLineComponent implements OnInit, OnDestroy {
+export class ControlScaleLineComponent implements OnInit, OnChanges, OnDestroy {
   instance: control.ScaleLine;
+
+  @Input() className?: string;
+  @Input() minWidth?: number;
+  @Input() render?: ((event: MapEvent) => any);
+  @Input() target?: Element;
+  @Input() units?: (control.ScaleLine.Units | string);
 
   constructor(private map: MapComponent) {
     // console.log('instancing aol-control-scaleline');
@@ -17,6 +23,22 @@ export class ControlScaleLineComponent implements OnInit, OnDestroy {
     this.instance = new control.ScaleLine(this);
     this.map.instance.addControl(this.instance);
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    let properties: { [index: string]: any } = {};
+    if (!this.instance) {
+      return;
+    }
+
+    for (let key in changes) {
+      if (changes.hasOwnProperty(key)) {
+        properties[key] = changes[key].currentValue;
+      }
+    }
+
+    this.instance.setProperties(properties, false);
+  }
+
 
   ngOnDestroy() {
     // console.log('removing aol-control-scaleline');
